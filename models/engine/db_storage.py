38018a1d8c077469adea2d7581809f35f90541a5
a-sowa <axelsowapro@gmail.com>
2024-04-11 11:14:06 +0000

    Task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -82,7 +82,7 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-            """
-            Call remove() method on the private session attribute
-            """
-            self.__session.remove()
+        """
+        Call remove() method on the private session attribute
+        """
+        self.__session.close()
